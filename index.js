47fde23376f8151d117ba8c59e562e30dc9f5f73
Timothy Lindsay <zodd@Timothys-MBP.lan>
2019-12-08 15:40:55 -0800

    rendered list and created line through for js file

---
+++ index.js
@@ -0,0 +1,25 @@
+function generateList(listItem){
+	return `<li><span class="shopping-item">${listItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`
+}
+
+function createShoppingList() {
+	$('#js-shopping-list-form').on('submit', function(e){
+		e.preventDefault();
+
+		const listItem = $('#shopping-list-entry').val();
+		//console.log(value);
+
+		$('.shopping-list').append($(generateList(listItem)));
+		//$('#shopping-list-entry').val('');
+  });
+};
+
+function lineThroughItem(){
+	$('.shopping-list').on('click', '.shopping-item-toggle', function(e){
+		e.preventDefault();
+		$(e.target).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+
+	})
+}
+$(createShoppingList)
+$(lineThroughItem)
